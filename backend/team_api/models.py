35b604f9b47a733db54374ce8b1879f49dc28c64
Alexander Arrico <alex.arrico@gmail.com>
2024-03-27 21:07:32 -0700

    fix[api]: cleanup model and add blank allowed to deletedAt field

--- backend/team_api/models.py
+++ backend/team_api/models.py
@@ -7,13 +7,19 @@ class Roles(Enum):
     REGULAR = 1
 
 
+ROLE_CHOICES = [(r.value, r.name) for r in Roles]
+
+
 class TeamMember(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
     phone = models.CharField(max_length=15)
     role = models.IntegerField(
-        choices=[(r.value, r.name) for r in Roles], default=Roles.REGULAR)
+        choices=ROLE_CHOICES, default=Roles.REGULAR)
     addedAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
-    deletedAt = models.DateTimeField()
+    deletedAt = models.DateTimeField(blank=True)
+
+    class Meta:
+        ordering = ["role", "first_name"]
