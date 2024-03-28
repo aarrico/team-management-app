561dd4f89ac23db923712b60d44c3b0292b8bd89
Alexander Arrico <alex.arrico@gmail.com>
2024-03-27 20:36:50 -0700

    fix[api]: use enum for Roles

--- backend/team_api/models.py
+++ backend/team_api/models.py
@@ -1,17 +1,19 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+from enum import Enum
+
+
+class Roles(Enum):
+    ADMIN = 0
+    REGULAR = 1
 
 
 class TeamMember(models.Model):
-    class Roles(models.IntegerChoices):
-        ADMIN = 0, _("admin")
-        REGULAR = 1, _("regular")
-
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
     phone = models.CharField(max_length=15)
-    role = models.IntegerField(choices=Roles, default=Roles.REGULAR)
+    role = models.IntegerField(
+        choices=[(r.value, r.name) for r in Roles], default=Roles.REGULAR)
     addedAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
     deletedAt = models.DateTimeField()
