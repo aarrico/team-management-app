9e638989e679906ef8a8d66dcb681d9643dd9cc0
Alexander Arrico <alex.arrico@gmail.com>
2024-03-28 11:18:19 -0700

    feat[api]: add simple model serializer

---
+++ backend/team_api/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework.serializers import ModelSerializer
+from .models import TeamMember
+
+class TeamMemberSerializer(ModelSerializer):
+    class Meta:
+        model = TeamMember
+        fields = ('id', 'first_name', 'last_name', 'email', 'phone', 'role')
