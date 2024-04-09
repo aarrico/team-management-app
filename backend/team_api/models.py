b3f6ecb6e954cb9dda713ccfcdebc7227b9dd651
Alexander Arrico <alex.arrico@gmail.com>
2024-04-08 17:00:38 -0700

    fix[be]: update destroy override to check for admin member count

--- backend/team_api/models.py
+++ backend/team_api/models.py
@@ -21,7 +21,7 @@ class TeamMember(models.Model):
     profile_pic_src = models.CharField(max_length=200, null=True)
 
     def __str__(self) -> str:
-        return f'{self.first_name} + {self.last_name}'
+        return f'{self.first_name} {self.last_name}'
         
     class Meta:
         db_table = 'team_member'
--- backend/team_api/views.py
+++ backend/team_api/views.py
@@ -1,4 +1,6 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
+from rest_framework import status
 from django.utils import timezone
 
 from .serializers import TeamMemberSerializer
@@ -9,14 +11,25 @@ class TeamMembersListCreate(ListCreateAPIView):
     queryset = TeamMember.objects.all().exclude(deleted_at__isnull=False)
     serializer_class = TeamMemberSerializer
 
+
 class TeamMemberGetUpdateDelete(RetrieveUpdateDestroyAPIView):
     queryset = TeamMember.objects.all()
     serializer_class = TeamMemberSerializer
 
-    def perform_destroy(self, instance):
+    def destroy(self, request, *args, **kwargs):
+        if not request.user.is_anonymous and request.user.role != TeamMember.Role.ADMIN:
+            return Response({'error': 'only admins can perform this operation'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        instance = self.get_object()
+
+        if instance.role == TeamMember.Role.ADMIN and TeamMember.objects.filter(role=TeamMember.Role.ADMIN).count() == 1:
+            return Response({'error': 'must have at least one admin user'}, status=status.HTTP_400_BAD_REQUEST)
+
         instance.deleted_at = timezone.now()
         instance.save()
-        
+
+        return Response({'message': f'{instance.first_name} {instance.last_name} successfully deleted!'})
+
     def perform_update(self, serializer):
         serializer.save(updated_at=timezone.now())
         return super().perform_update(serializer)
